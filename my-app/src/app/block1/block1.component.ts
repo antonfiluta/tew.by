3328b3bdda9d226c12369eba942bedc497f29d56
antonfiluta <antonfiluta2.0@mail.ru>
2024-06-20 10:02:21 +0300

    feat: work

---
+++ my-app/src/app/block1/block1.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'block1-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './block1.component.html',
+  styleUrl: './block1.component.css'
+})
+export class block1Component {
+  
+}
